f5bc7db2f04d825b655d1579ccbbd4f48bc5c2b5
Dhruv-Raichand <dhruvraichand70@gmail.com>
2025-07-12 14:01:15 +0530

    handling routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-app.use("/test", (req, res) => {
-    res.send("Hnji Beta")
+app.use("/hello/2", (req, res) => {
+    res.send("Hello Beta 2 baar")
 })
 app.use("/hello", (req, res) => {
     res.send("Hello Beta")
 })
+app.use("/test", (req, res) => {
+    res.send("Hnji Beta")
+})
+// app.use("/", (req, res) => {
+//     res.send("hnji ki chahida")
+// })
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
 })
