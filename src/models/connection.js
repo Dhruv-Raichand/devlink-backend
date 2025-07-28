3be15354f7fea51f2e6e2bb81c9b6adc2cbe0bbc
Dhruv-Raichand <dhruvraichand70@gmail.com>
2025-07-28 22:18:16 +0530

    Fix: Ensure required fields for fromUserId and toUserId in connection schema

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,12 +3,15 @@ const mongoose  = require("mongoose");
 const connectionSchema = mongoose.Schema({
     fromUserId: {
         type: mongoose.Schema.Types.ObjectId,
+        required: true
     },
     toUserId: {
         type: mongoose.Schema.Types.ObjectId,
+        required: true
     },
     status: {
         type: String,
+        required: true,
         enum: {
             values: ["interested", "ignored", "accepted", "rejected"],
             mesaage: "{VALUE} is not a valid status"
