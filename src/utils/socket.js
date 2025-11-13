e686bd60ae6a15f0e2c1081db9521196fe42e161
Dhruv-Raichand <dhruvraichand70@gmail.com>
2025-11-13 21:14:40 +0530

    Implement chat functionality with chat model and router, and integrate message saving in socket events

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const Chat = require("../models/chat");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -21,11 +22,42 @@ const initializeSocket = (server) => {
       console.log(firstName + " Joining Room: " + roomId);
       socket.join(roomId);
     });
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      console.log(firstName + ": " + text);
-      io.to(roomId).emit("messageReceived", { firstName, text });
-    });
+    socket.on(
+      "sendMessage",
+      async ({ firstName, lastName, photoUrl, userId, targetUserId, text }) => {
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+          console.log(firstName + ": " + text);
+
+          //save to DB
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+
+          await chat.save();
+          io.to(roomId).emit("messageReceived", {
+            firstName,
+            lastName,
+            photoUrl,
+            text,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    );
     socket.on("disconnect", () => {});
   });
 };
