d7a4a3c60156b33cacceccfa48dbbe7458789d2c
Dhruv-Raichand <dhruvraichand70@gmail.com>
2026-02-21 13:53:00 +0530

    Convert user routes to TypeScript

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,7 +4,7 @@ const connectionRequest = require("../models/connection");
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
 
-userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+userRouter.get("/user/requests/received", userAuth, async (req:any, res:any): Promise<void> => {
   try {
     const SAFE_USER_FIELDS =
       "_id firstName lastName about photoUrl skills age gender";
@@ -15,7 +15,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     if (requests.length === 0) {
       throw new Error("No Recieved Requests!!!");
     }
-    const received = requests.map(({ _id, fromUserId }) => ({
+    const received = requests.map(({ _id, fromUserId }: any) => ({
       _id,
       fromUserId,
     }));
@@ -23,14 +23,14 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
       message: "received requests",
       data: received,
     });
-  } catch (err) {
+  } catch (err: any) {
     res.status(400).json({
       message: err.message,
     });
   }
 });
 
-userRouter.get("/user/connections", userAuth, async (req, res) => {
+userRouter.get("/user/connections", userAuth, async (req:any, res:any): Promise<void> => {
   try {
     const loggedInUser = req.user;
     const SAFE_USER_FIELDS =
@@ -47,7 +47,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     if (connections.length === 0) {
       throw new Error("No Connections");
     }
-    const connectionUsers = connections.map((row) => {
+    const connectionUsers = connections.map((row: any) => {
       if (loggedInUser._id.toString() === row.toUserId._id.toString()) {
         return row.fromUserId;
       } else {
@@ -58,29 +58,34 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       message: "Connections",
       data: connectionUsers,
     });
-  } catch (err) {
+  } catch (err:any) {
     res.status(400).json({
       message: err.message,
     });
   }
 });
 
-userRouter.get("/feed", userAuth, async (req, res) => {
+userRouter.get("/feed", userAuth, async (req: any, res: any): Promise<void> => {
   try {
     const loggedInUser = req.user;
-    const page = req.query.page || 1;
-    let limit = req.query.limit || 10;
+
+    const page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
     limit = limit > 50 ? 50 : limit;
     const skip = (page - 1) * limit;
+
     const SAFE_USER_FIELDS =
       "_id firstName lastName about photoUrl skills age gender";
+
     const connectionRequests = await connectionRequest
       .find({
         $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
       })
       .select("fromUserId toUserId");
-    const hideUserFromFeed = new Set();
-    connectionRequests.forEach((req) => {
+
+    const hideUserFromFeed = new Set<string>();
+    connectionRequests.forEach((req: any) => {
       hideUserFromFeed.add(req.toUserId.toString());
       hideUserFromFeed.add(req.fromUserId.toString());
     });
@@ -97,7 +102,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       message: "new Users ",
       data: Users,
     });
-  } catch (err) {
+  } catch (err:any) {
     res.status(400).json({
       message: err.message,
     });
@@ -105,3 +110,4 @@ userRouter.get("/feed", userAuth, async (req, res) => {
 });
 
 module.exports = userRouter;
+export{};
