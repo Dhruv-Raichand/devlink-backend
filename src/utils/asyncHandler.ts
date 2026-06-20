f017ec05c52bae45c86aaac00927c2ab2357232e
Dhruv-Raichand <dhruvraichand70@gmail.com>
2026-06-20 21:39:33 +0530

    fix(asyncHandler): correct ReqBody typo in generic parameter

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
 
 export const asyncHandler =
-  (
-    fn: (...args: Parameters<RequestHandler>) => Promise<void>
-  ): RequestHandler =>
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+  <P = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = ParsedQs>(
+    fn: (
+      req: Request<P, ResBody, ReqBody, ReqQuery>,
+      res: Response<ResBody>,
+      next: NextFunction
+    ) => Promise<void>
+  ): RequestHandler<P, ResBody, ReqBody, ReqQuery> =>
+  (req, res, next) => {
+    Promise.resolve(
+      fn(req as Request<P, ResBody, ReqBody, ReqQuery>, res, next)
+    ).catch(next);
   };
